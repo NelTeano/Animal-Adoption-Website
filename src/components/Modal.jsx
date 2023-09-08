92ef2c2ffea3f0a48942fa5a01af9ccdfae6793a
JONEL <jonelteano29@gmail.com>
2023-09-08 20:58:13 +0800

    add modal component & put modalGuard in FillupPage(prevent to continue if not logged in)

---
+++ src/components/Modal.jsx
@@ -0,0 +1,70 @@
+import { Link } from "react-router-dom";
+import PropTypes from 'prop-types';
+
+
+export default function Modal({ isOpen, closeModal, children }) {
+
+
+
+    const modalOverlayStyle = {
+        position: "fixed",
+        top: 0,
+        left: 0,
+        width: "100%",
+        height: "100%",
+        backgroundColor: "rgba(0, 0, 0, 0.5)",
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center",
+        zIndex: 1000,
+    };
+    
+    const modalStyle = {
+        backgroundColor: "white",
+        padding: "80px",
+        borderRadius: "5px",
+        boxShadow: "0 2px 10px rgba(0, 0, 0, 0.2)",
+        position: "relative",
+        zIndex: 1001,
+        width: "80%",
+        maxWidth: "400px",
+        textAlign: "center",
+    };
+    
+    const closeButtonStyle = {
+        position: "absolute",
+        top: "0px",
+        right: "15px",
+        background: "none",
+        border: "none",
+        fontSize: "40px",
+        cursor: "pointer",
+        padding: "0",
+        outline: "none",
+    };    
+
+
+    return (
+        isOpen && (
+        <div style={modalOverlayStyle}>
+            <div style={modalStyle}>
+                <Link
+                    to="/gallery"
+                >
+                    <button style={closeButtonStyle} onClick={closeModal}>
+                        &times;
+                    </button>
+                </Link>
+            {children}
+            </div>
+        </div>
+        )
+    );
+}
+
+
+Modal.propTypes = {
+    isOpen: PropTypes.bool.isRequired,
+    closeModal: PropTypes.func.isRequired,
+    children: PropTypes.node.isRequired,
+};
--- src/pages/FillUpPage.jsx
+++ src/pages/FillUpPage.jsx
@@ -1,42 +1,29 @@
 import { useEffect, useState } from "react";
 import { Link, useParams } from "react-router-dom";
 import { Image, Button} from "@chakra-ui/react";
+import { useAuth0} from "@auth0/auth0-react";
 
+// COMPONENTS
+import Modal from "../components/Modal"
+import AuthButton from "../components/AuthButton"
 
 // STYLES
 import "../assets/styles/fillupStyle.css"
 
 
-export default function FillUpPage() {
 
-    const AnimalId = useParams(); // TO ACCESS THE ID OF ANIMAL USING THE PARAMS 
-    console.log(AnimalId) // TO VERIFY IF THE ANIMAL ID RECEIVED IS CORRECT
+function FillUpPage() {
 
+    // PAGE STATE
     const [ ChosenAnimal , setChosenAnimal ] = useState(''); // FOR STORING THE CHOSENANIMAL DATA
+    const [modalOpen, setModalOpen] = useState(true); // TO BE ABLE CLOSE AND OPEN THE MODAL
 
-    useEffect(()=>{
-        
-    async function GetAnimalByParams(){
-        
-        try{
-            const data = await fetch(`http://localhost:5174/api/animals/${AnimalId.id}`, {
-                headers: {
-                    Accept: 'application/json',
-                },
-            })
-            const jsonAnimalData = await data.json();
-            setChosenAnimal(jsonAnimalData)
-        }catch(err){
-            console.log('Cant Fetch the Animal Data');
-        }  
-    }   
-
-    GetAnimalByParams(); // FETCH THE ROUTE USING PARAMS ROUTE IS USING findById()
-    },[])
-
-    console.log(ChosenAnimal)   // TO VERIFY THE CHOSEN ANIMAL DATA IS CORRECT
+    // USE FUNCTIONS FROM IMPORTS
+    const { isAuthenticated } = useAuth0();
+    const AnimalId = useParams(); // TO ACCESS THE ID OF ANIMAL USING THE PARAMS 
 
 
+    // PAGE VARIABLES
     const ButtonsStyle = {
         backgroundColor: "#ADA7FF",
         height: "100px",
@@ -49,43 +36,95 @@ export default function FillUpPage() {
         }
     }
 
+    useEffect(()=>{
+        async function GetAnimalByParams(){
+            
+            try{
+                const data = await fetch(`http://localhost:5174/api/animals/${AnimalId.id}`, {
+                    headers: {
+                        Accept: 'application/json',
+                    },
+                })
+                const jsonAnimalData = await data.json();
+                setChosenAnimal(jsonAnimalData)
+            }catch(err){
+                console.log('Cant Fetch the Animal Data');
+            }  
+        }   
+    
+    GetAnimalByParams(); // FETCH THE ROUTE USING PARAMS ROUTE IS USING findById()
+    },[])
+
+
+    if(!isAuthenticated){
+
+        const closeModal = () => {
+            setModalOpen(false);
+        };
+
+        return(
+            <>
+                    
+                <Modal isOpen={modalOpen} closeModal={closeModal}>
+                    <div>     
+                        <h2>Please Log in</h2>
+                        <br></br>
+                        <p>
+                            Need to Sign in First To Continue<br></br> your adopting process
+                        </p>
+                        <br></br>
+                        <AuthButton/>
+                    </div>
+                </Modal>
+            </>
+        )
+    }
+
+    
+
+    console.log(ChosenAnimal)   // TO VERIFY THE CHOSEN ANIMAL DATA IS CORRECT
+    console.log(AnimalId) // TO VERIFY IF THE ANIMAL ID RECEIVED IS CORRECT
+
+
 
     return (
         <>
             {ChosenAnimal && // JUST SAMPLE
-                <>
-                    <div className="information-board">
-                        <div className="animal-appearance">
-                            <Image 
-                                src={ChosenAnimal.animal_image}
-                            />
-                            <div className="animal-label">
-                                <h2>Name : {ChosenAnimal.name}</h2>
-                                <h3>Breed : {ChosenAnimal.breed}</h3>
-                            </div>
-                        </div>
-                        <div className="animal-information">
-                                <div className="information-details">
-                                    <h2>About :  </h2>
-                                    <h3>Name : {ChosenAnimal.name}</h3>
-                                    <h3>Age : {ChosenAnimal.age}</h3>
-                                    <h3>Animal Type : {ChosenAnimal.animal_type}</h3>
-                                    <h3>Breed : {ChosenAnimal.breed}</h3>
-                                    <h3>Last Owner : {ChosenAnimal.last_owner}</h3>
-                                    <h3>Location : {ChosenAnimal.location}</h3>
-                                </div>
-                                <div className="information-buttons">
-                                    <Button sx={ButtonsStyle} > Apply For This Dog</Button>
-                                    <Link
-                                        to={"/gallery"}
-                                    >
-                                        <Button sx={ButtonsStyle} >Choose Other</Button>
-                                    </Link>
-                                </div>
+                <div className="information-board">
+                    <div className="animal-appearance">
+                        <Image 
+                            src={ChosenAnimal.animal_image}
+                        />
+                        <div className="animal-label">
+                            <h2>Name : {ChosenAnimal.name}</h2>
+                            <h3>Breed : {ChosenAnimal.breed}</h3>
                         </div>
                     </div>
-                </>
+                    <div className="animal-information">
+                            <div className="information-details">
+                                <h2>About :  </h2>
+                                <h3>Name : {ChosenAnimal.name}</h3>
+                                <h3>Age : {ChosenAnimal.age}</h3>
+                                <h3>Animal Type : {ChosenAnimal.animal_type}</h3>
+                                <h3>Breed : {ChosenAnimal.breed}</h3>
+                                <h3>Last Owner : {ChosenAnimal.last_owner}</h3>
+                                <h3>Location : {ChosenAnimal.location}</h3>
+                            </div>
+                            <div className="information-buttons">
+                                <Button sx={ButtonsStyle} > Apply For This Dog</Button>
+                                <Link
+                                    to={"/gallery"}
+                                >
+                                    <Button sx={ButtonsStyle} >Choose Other</Button>
+                                </Link>
+                            </div>
+                    </div>
+                </div>
             }
         </>
     )
 }
+
+
+export default FillUpPage
+
